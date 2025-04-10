69e4b2c3cf0b6e24649adf333a76ae51f8580cdc
Sergey <molotov9500@gmail.com>
2025-04-10 18:18:50 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     // <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/travel_logistics_analytics_service_frontend_react">
         <App/>
     </BrowserRouter>
     // </React.StrictMode>
